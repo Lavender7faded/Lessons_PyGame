2f377eb11c13d9416c0db900b783f9bb26973446
Anna Khishchenko <ann.kh18@gmail.com>
2021-07-28 22:04:55 +0300

    Квадрат який розширюється з центру до країв

--- PyGame_Lesson_2/magnification_of_square.py
+++ PyGame_Lesson_2/magnification_of_square.py
@@ -9,12 +9,12 @@ size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
-green_Color = (130, 183, 75)
-brown_Color = (193, 148, 106)
+Deep_Purplish_Pink_Color = (222, 111, 161)
+butterywhite_Color = (241, 235, 218)
 
 fps = 60
 def draw(x, y, a, b):
-    pygame.draw.rect(screen, green_Color, ((x, y), (a, b)))
+    pygame.draw.rect(screen, Deep_Purplish_Pink_Color, ((x, y), (a, b)))
 
 pos_x = 250
 pos_y = 250
@@ -28,12 +28,12 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     
-    screen.fill(brown_Color)
+    screen.fill(butterywhite_Color)
     draw(pos_x, pos_y, p_a, p_b)
     pos_x = pos_x - 1
     pos_y = pos_y - 1
-    p_a = p_a + 3
-    p_b = p_b + 3
+    p_a = p_a + 2
+    p_b = p_b + 2
 
     clock.tick(fps)
 
