9d587346884c80e6708639684129fa592aa7d274
Khischenko Anna <ann.kh18@gmail.com>
2021-08-25 14:25:13 +0300

    Доробила заадчу по збільшеню круга

--- PyGame_Lesson_3/big_circle.py
+++ PyGame_Lesson_3/big_circle.py
@@ -1,14 +1,13 @@
-# Напишите программу, в которой по щелчку левой кнопкой мышки в месте
-# щелчка появляется красный круг, постоянно растущий и увеличивающийся в
-# диаметре.
+# # Напишите программу, в которой по щелчку левой кнопкой мышки в месте
+# # щелчка появляется красный круг, постоянно растущий и увеличивающийся в
+# # диаметре.
 
 import pygame
 
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-fps = 30
-
+fps = 120
 MintCream_COLOR = (245, 255, 250)
 LightSkyBlue_COLOR = (135, 206, 250)
 
@@ -20,6 +19,7 @@ r = 0
 
 running = True
 pygame.display.update()
+click = False
 
 while running:
     screen.fill(MintCream_COLOR)
@@ -27,14 +27,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x, y = event.pos[0], event.pos[1]
-            
-    draw(r)
-    r = r + 1 
+            r=0
+            click = True
+            x, y = event.pos
+
+    if click == True:
+        draw(r)
+        r = r + 1
 
     pygame.display.flip()
     clock.tick(fps)
 
 pygame.quit()
-
-
